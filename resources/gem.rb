0285dbadbea9283520186efdd35652558ded5bd1
Benjamin Fischer <you@example.com>
2018-05-29 11:34:22 +0200

    nits

--- resources/gem.rb
+++ resources/gem.rb
@@ -1,6 +1,6 @@
 default_action :install
 
-property :gem, [String]
+property :package_name, [String]
 property :version, [String, NilClass], default: nil
 property :user, [String], default: 'root'
 property :ruby_string, [String], required: true
@@ -11,22 +11,22 @@ action :install do
     rvm.gemset_create(ruby_string)
   end
 
-  if rvm.gem?(ruby_string, gem, version)
-    Chef::Log.debug("Gem #{gem} #{version} already installed on gemset #{ruby_string} for user #{user}.")
+  if rvm.gem?(ruby_string, package_name, version)
+    Chef::Log.debug("Gem #{package_name} #{version} already installed on gemset #{ruby_string} for user #{user}.")
   else
-    Chef::Log.debug("Install gem #{gem} #{version} on gemset #{ruby_string} for user #{user}.")
-    rvm.gem_install(ruby_string, gem, version)
+    Chef::Log.debug("Install gem #{package_name} #{version} on gemset #{ruby_string} for user #{user}.")
+    rvm.gem_install(ruby_string, package_name, version)
     updated_by_last_action(true)
   end
 end
 
 %i[update uninstall].each do |action_name|
   action action_name do
-    if rvm.gem?(ruby_string, gem, version)
-      Chef::Log.debug "#{action_name.to_s.capitalize} gem #{gem} #{version} from gemset #{ruby_string} for user #{user}."
+    if rvm.gem?(ruby_string, package_name, version)
+      Chef::Log.debug "#{action_name.to_s.capitalize} gem #{package_name} #{version} from gemset #{ruby_string} for user #{user}."
       updated_by_last_action(true)
     else
-      Chef::Log.debug "Gem #{gem} #{version} is not installed on gemset #{ruby_string} for user #{user}."
+      Chef::Log.debug "Gem #{package_name} #{version} is not installed on gemset #{ruby_string} for user #{user}."
     end
   end
 end
